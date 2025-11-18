2b248f7e3f11b6b10eee903f8d8c456c5ec4e2eb
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-11-18 11:34:53 -0800

    :sparkles: feat: implement update and remove methods to Coffees Controller

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -16,5 +24,16 @@ export class CoffeesController {
   create(@Body() body: any) {
     console.log(body);
     return 'This action creates a coffee.';
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: any) {
+    console.log(body);
+    return `This action updates #${id} coffee.`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee.`;
   }
 }
