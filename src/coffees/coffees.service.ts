b95c6c9de4da285c361b46086199a29ef21479b8
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-11-20 07:33:33 -0800

    :sparkles: feat: enhance coffee retrieval methods to include related flavors

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -13,12 +13,17 @@ export class CoffeesService {
   ) {}
 
   findAll() {
-    return this.coffeeRepository.find();
+    return this.coffeeRepository.find({
+      relations: ['flavors'],
+    });
   }
 
   async findOne(id: number) {
-    const coffee = await this.coffeeRepository.findOneBy({
-      id: id,
+    const coffee = await this.coffeeRepository.findOne({
+      where: {
+        id: id,
+      },
+      relations: ['flavors'],
     });
     if (!coffee) {
       throw new NotFoundException(`Coffee #${id} not found!`);
