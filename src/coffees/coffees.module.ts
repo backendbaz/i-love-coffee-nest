e390bca20adaafde0d7edc7b6da3ec83f079dd90
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-11-22 00:53:46 -0800

    :sparkles: feat: Class Providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,12 +6,24 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
+import * as process from 'node:process';
+
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentConfigService
+          : ProductionConfigService,
+    },
     {
       provide: COFFEE_BRANDS,
       useValue: ['buddy brew', 'nescafe'],
